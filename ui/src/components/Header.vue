3b6f16394343aebaa7cdcf0124b5f8c192744a44
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-10-06 21:09:15 -0500

    added template for header

---
+++ ui/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark">
+      <span class="logo">MARYVILLE GUIDE</span>
+      <button
+        class="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#expandNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="expandNav">
+        <div class="navbar-nav ml-auto">
+          <router-link to="/" tag="button">
+            <button class="button">Home</button>
+          </router-link>
+          <router-link to="/login" tag="button">
+            <button class="button">Sign Up</button>
+          </router-link>
+          <router-link to="/login">
+            <button class="button">Login</button>
+          </router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
