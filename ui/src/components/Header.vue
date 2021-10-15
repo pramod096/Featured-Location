564940510216e937686b552b73457f649b8c2fd8
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-10-14 21:45:23 -0500

    shadow for app logo

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -66,4 +66,12 @@ export default {
 .navbar-dark .navbar-toggler-icon {
   background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(1,1,1, 2)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
 }
+
+.logo {
+  color: #363233;
+  font-family: Luminari, fantasy;
+  font-weight: 100;
+  text-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
 </style>
