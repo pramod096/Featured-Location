bf2af92bc4df4abe8f2fd781e0fe9da0f7d27962
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-10-14 20:49:32 -0500

    Added Style card to single list

--- ui/src/components/SingleList.vue
+++ ui/src/components/SingleList.vue
@@ -16,3 +16,18 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.card {
+  height: max-content;
+  font-weight: bold;
+  font-size: 1.8rem;
+  border: none;
+  outline: none;
+  border-radius: 22px;
+  background-color: #e0e5ec;
+  box-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
+
+</style>
