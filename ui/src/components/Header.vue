df2bd4d907622e78898c65163ebf8a285a6c9684
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-10-14 21:43:21 -0500

    removed outline and border for navbar

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -56,4 +56,10 @@ export default {
   box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.6),
     inset -9px -9px 16px rgba(255, 255, 255, 0.5);
 }
+
+.navbar-toggler:focus,
+.navbar-toggler:active {
+  outline: none;
+  border: none;
+}
 </style>
