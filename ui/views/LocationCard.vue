54d105884219b1218b0ce06528ef1aa232ebbaf0
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-10-14 23:44:10 -0500

    Added styles for the like button.

--- ui/views/LocationCard.vue
+++ ui/views/LocationCard.vue
@@ -82,4 +82,9 @@ export default {
     -9px -9px 16px rgba(255, 255, 255, 0.5);
 }
 
+.btn:hover {
+  box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.6),
+    inset -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
+
 </style>
