af21778dd5e476d1c0a1031aaae8f14fc357583e
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-10-14 21:43:56 -0500

    changed the background color of icon in navbar

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -62,4 +62,8 @@ export default {
   outline: none;
   border: none;
 }
+
+.navbar-dark .navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(1,1,1, 2)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+}
 </style>
