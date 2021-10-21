f1fdc78715d4f1d7ad2b4752a467fef3d3d36950
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-10-20 21:41:50 -0500

    data and methods for allLocations

--- ui/views/AllLocations.vue
+++ ui/views/AllLocations.vue
@@ -5,3 +5,40 @@
     </div>
   </div>
 </template>
+
+<script>
+import LocationService from "../LocationService";
+import SingleList from "../components/SingleList.vue";
+export default {
+  components: { SingleList },
+  name: "AllLocations",
+  data() {
+    return {
+      titles: [
+        "NODAWAY COUNTY SOCIETY MUSEUM",
+        "MOZINGO LAKE",
+        "BACKYARD VINE",
+        "BEARCAT STADIUM",
+        "MARYVILLE BOARD GAME CAFE",
+        "BEARCAT LANES",
+      ],
+    };
+  },
+
+  async created() {
+    let titles = null;
+    try {
+      titles = await LocationService.getLocations();
+      titles = titles.data;
+      console.log(titles);
+    } catch (err) {
+      console.log(err);
+    }
+
+    for (let i = 0; i < titles.length; i++) {
+      this.titles.push(titles[i].name);
+      console.log(this.titles);
+    }
+  },
+};
+</script>
