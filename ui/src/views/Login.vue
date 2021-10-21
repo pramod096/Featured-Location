41e6a9efb72b6a2edf10acdf12133556e45923f9
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-10-20 23:34:13 -0500

    Added buttons to the style

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -82,4 +82,25 @@ export default {
     inset -9px -9px 16px rgba(255, 255, 255, 0.5);
   transition: box-shadow 500ms;
 }
+
+.button {
+  width: 25rem;
+  font-weight: bold;
+  padding: 1rem;
+  font-size: 1.1rem;
+  margin: 2rem 0rem;
+  border: none;
+  outline: none;
+  color: #007bff;
+  border-radius: 22px;
+  background-color: #e0e5ec;
+  box-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+  transition: box-shadow 500ms;
+}
+
+.button:hover {
+  box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.6),
+    inset -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
 </style>
