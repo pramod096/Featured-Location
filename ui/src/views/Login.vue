c0f36e35d4ec7f9ff297ca8f5a52b3394c7c2538
Narendra-kumar-Gunturu <77824814+Narendra-kumar-Gunturu@users.noreply.github.com>
2021-10-20 23:11:28 -0500

    Added email and password text fields

---
+++ ui/src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="container text-center">
+    <div class="card mx-auto">
+      <div class="card-header">
+        <span>LOGIN</span>
+      </div>
+      <form>
+        <div class="form-group">
+          <label for="email">Email ID</label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted"
+            >We'll never share your email with anyone else.</small
+          >
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Enter Password"
+          />
+        </div>
+</template>
