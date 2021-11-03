ccac02f923fcaf9338a9b82763ec2ee8b9a01e80
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2021-11-03 00:44:45 -0500

    created footer vue

---
+++ ui/src/components/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container row text-center">
+    <div class="col-sm-3">
+      <a
+        class="link"
+        href="https://github.com/pramod096/Featured-Location"
+        target="_blank"
+        >Source Repo</a
+      >
+    </div>
+    <div class="col-sm-3">
+      <a
+        class="link"
+        href="https://github.com/pramod096/Proposal-4B/projects/1"
+        target="_blank"
+        >Work Plan</a
+      >
+    </div>
+    <div class="col-sm-3">
+      <a
+        class="link"
+        href="https://github.com/pramod096/Proposal-4B/blob/main/Proposal.md"
+        target="_blank"
+      >
+        Proposal</a
+      >
+    </div>
+    <div class="col-sm-3 link">Group: 4B</div>
+  </div>
+</template>
+
+<style scoped>
+.col-sm-3 {
+  font-size: 0.9rem;
+  font-family: Luminari, fantasy;
+  text-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+  text-align: center;
+}
+
+.link {
+  text-decoration: none;
+  color: #363233;
+}
+
+.link:hover {
+  color: #007bff;
+}
+
+.container {
+  padding: 2%;
+}
+</style>
