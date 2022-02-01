c48ce2654a2d0afb6bfc68ea0e7f923bc5390351
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-02-01 16:11:59 -0600

    ui to upload images

---
+++ ui/src/views/imageupload.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="/upload" method="POST" enctype="multipart/form-data">
+    <input type="file" name="photo" id="file" />
+    <input type="text" name="fname" />
+    <input type="submit" value="Submit" />
+  </form>
+</template>
+
+<script>
+import LocationService from "../LocationService";
+import router from "@/router";
+
+export default {
+  name: "imageUpload",
+  data() {
+    return {
+      fName: "",
+      photo: null,
+    };
+  },
+  methods: {
+    async submit() {
+      await LocationService.postLocation();
+    },
+  },
+};
+</script>
+
