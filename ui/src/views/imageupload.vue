7531fc48de6f43019d6a8e4a266dd98e98193c93
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-02-01 16:38:53 -0600

    updated image upload

--- ui/src/views/imageupload.vue
+++ ui/src/views/imageupload.vue
@@ -1,28 +1,19 @@
 <template>
+<div>
   <form action="/upload" method="POST" enctype="multipart/form-data">
     <input type="file" name="photo" id="file" />
     <input type="text" name="fname" />
     <input type="submit" value="Submit" />
   </form>
+</div>
 </template>
 
 <script>
-import LocationService from "../LocationService";
-import router from "@/router";
+
 
 export default {
   name: "imageUpload",
-  data() {
-    return {
-      fName: "",
-      photo: null,
-    };
-  },
-  methods: {
-    async submit() {
-      await LocationService.postLocation();
-    },
-  },
+
 };
 </script>
 
