4f8a9aedbc98c3d7178c454276f8cf389c7ff832
AbhiRam0099 <abhilashramvaram@gmail.com>
2022-02-11 20:38:37 -0600

    global variable for auth0

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from "vue";
+import App from "./App.vue"
 import router from './router'
+import createAuth0Client from "@auth0/auth0-spa-js";
+import config from "../auth_config.json";
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.config.globalProperties.$auth0 = createAuth0Client({
+    domain: config.domain,
+    client_id: config.clientId,
+  });
+
+
+app.use(router).mount('#app')
