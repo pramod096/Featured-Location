c3d1487a1b4a2899db9d848953aa6ff13a8bb1fe
Pramod Reddy Gonegari <s541903@nwmissouri.edu>
2022-02-16 10:32:00 -0600

    UI to add location

---
+++ ui/src/views/AddLocation.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container text-center">
+    <div class="card mx-auto">
+      <div class="card-header">
+        <span>ADD LOCATION</span>
+      </div>
+      <form
+        @submit.prevent="submit"
+        method="POST"
+        enctype="multipart/form-data"
+        id="locationForm"
+      >
+        <div class="form-group">
+          <label for="locationName">Location Name</label>
+          <input
+            v-model="locationName"
+            type="text"
+            class="form-control"
+            id="locationName"
+            name="locationName"
+            placeholder="Location Name"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="address">Address</label>
+          <input
+            v-model="address"
+            type="text"
+            class="form-control"
+            id="address"
+            name="address"
+            placeholder="Address"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="hours">Hours</label>
+          <input
+            v-model="hours"
+            type="text"
+            class="form-control"
+            id="hours"
+            name="hours"
+            placeholder="Hours"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="phoneNumber">Phone Number</label>
+          <input
+            v-model="phoneNumber"
+            type="number"
+            class="form-control"
+            id="phoneNumber"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="photo">Upload Image</label>
+          <input
+            type="file"
+            class="form-control"
+            name="photo"
+            id="photo"
+            required
+          />
+        </div>
+        <button class="button" type="submit">Save</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+
+
+
