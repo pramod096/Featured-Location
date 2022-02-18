a1fc7fc6b9728b6b3700b9fdd0a4f1a6668372d8
pramod096 <pramod096@users.noreply.github.com>
2022-02-18 17:08:28 +0000

    Prettified Code!

--- server/index.js
+++ server/index.js
@@ -1,29 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const locations = require('./routes/api/locations');
-const users = require('./routes/api/users')
+const locations = require("./routes/api/locations");
+const users = require("./routes/api/users");
 
-app.use('/api/locations', locations);
-app.use('/api/users', users);
-
-
+app.use("/api/locations", locations);
+app.use("/api/users", users);
 
 //For production
-if(process.env.NODE_ENV === 'production'){
-    
-    //Static folder
-    app.use(express.static(__dirname + '/public/'));
+if (process.env.NODE_ENV === "production") {
+  //Static folder
+  app.use(express.static(__dirname + "/public/"));
 
-    //For SPA
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  //For SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 const port = process.env.PORT || 8000;
--- server/routes/api/locations.js
+++ server/routes/api/locations.js
@@ -1,77 +1,78 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express = require("express");
+const mongodb = require("mongodb");
 
-require('dotenv').config();
-const multer = require('multer')
-// const upload = multer({ dest: __dirname + '/uploads'})	
+require("dotenv").config();
+const multer = require("multer");
+// const upload = multer({ dest: __dirname + '/uploads'})
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage: storage }).single('photo');	
-const { uploader } = require('cloudinary')	
-const cloudinary = require('cloudinary')	
-require('dotenv').config();	
-cloudinary.config({	
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,	
-  api_key: process.env.CLOUDINARY_API_KEY,	
-  api_secret: process.env.CLOUDINARY_API_SECRET	
+const multerUploads = multer({ storage: storage }).single("photo");
+const { uploader } = require("cloudinary");
+const cloudinary = require("cloudinary");
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const router = express.Router();
 
-
 //Get Locations
-router.get('/', async (req, res) => {
-    const locations =  await getLocationsCollection();
-    res.send(await locations.find({}).toArray());
+router.get("/", async (req, res) => {
+  const locations = await getLocationsCollection();
+  res.send(await locations.find({}).toArray());
 });
 
-router.post('/', multerUploads, async (req, res) => {
-  if(req.file) {
-    const locations =  await getLocationsCollection();
-    const file = 'data:image/png;base64,' + (req.file.buffer).toString('base64')	
-  return uploader.upload(file).then(async (result) => {
-  const image = result.url;
-  const locations =  await getLocationsCollection();
-    await locations.insertOne({
-      locationName: req.body.locationName,
-      address: req.body.address,	
-          hours: req.body.hours,
-          phoneNumber: req.body.phoneNumber,	
-          photo: image
-    });
-    return res.status(200).json({
-      messge: 'Your image has been uploded successfully to cloudinary',
-  data: {	
-  image	
-  }
-  })	
-  }).catch((err) => res.status(400).json({	
-  messge: 'someting went wrong while processing your request',
-  data: {	
-  err	
-  }	
-  }))	
-  }
-  });
-
-  router.delete('/', async (req, res) => {
+router.post("/", multerUploads, async (req, res) => {
+  if (req.file) {
     const locations = await getLocationsCollection();
-    await locations.deleteOne({
-      _id: req.body._id,
-    });
-    res.status(201).send();
-  });
+    const file = "data:image/png;base64," + req.file.buffer.toString("base64");
+    return uploader
+      .upload(file)
+      .then(async (result) => {
+        const image = result.url;
+        const locations = await getLocationsCollection();
+        await locations.insertOne({
+          locationName: req.body.locationName,
+          address: req.body.address,
+          hours: req.body.hours,
+          phoneNumber: req.body.phoneNumber,
+          photo: image,
+        });
+        return res.status(200).json({
+          messge: "Your image has been uploded successfully to cloudinary",
+          data: {
+            image,
+          },
+        });
+      })
+      .catch((err) =>
+        res.status(400).json({
+          messge: "someting went wrong while processing your request",
+          data: {
+            err,
+          },
+        })
+      );
+  }
+});
 
+router.delete("/", async (req, res) => {
+  const locations = await getLocationsCollection();
+  await locations.deleteOne({
+    _id: req.body._id,
+  });
+  res.status(201).send();
+});
 
 async function getLocationsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    process.env.ATLAS_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+    }
+  );
 
-    const client = await mongodb.MongoClient.connect(
-      process.env.ATLAS_CONNECTION_STRING,
-        {
-          useNewUrlParser: true
-        }
-      );
-    
-      return client.db('FeaturedLocation').collection('locations');
-
+  return client.db("FeaturedLocation").collection("locations");
 }
 module.exports = router;
