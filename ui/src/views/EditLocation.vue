7bebed0d35ef7cb49d5a7ee7ce4281a56410516e
AbhiRam0099 <abhilashramvaram@gmail.com>
2022-02-22 17:02:42 -0600

    created styles for editlocation

--- ui/src/views/EditLocation.vue
+++ ui/src/views/EditLocation.vue
@@ -97,3 +97,68 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.card {
+  width: 26rem;
+  height: fit-content;
+  font-weight: bold;
+  font-size: 0.9rem;
+  padding: 0.5rem;
+  margin-bottom: 2rem;
+  border: none;
+  outline: none;
+  border-radius: 22px;
+  background-color: #e0e5ec;
+  box-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
+
+.card-header {
+  font-weight: 100;
+  font-family: Luminari, fantasy;
+  font-size: 1.8rem;
+  border: none;
+  outline: none;
+  border-top-left-radius: 22px;
+  border-top-right-radius: 22px;
+  background-color: #e0e5ec;
+  margin: 2rem 0rem;
+}
+
+.form-control {
+  width: 25rem;
+  height: max-content;
+  font-weight: bold;
+  padding: 1rem;
+  font-size: 1.1rem;
+  border: none;
+  outline: none;
+  border-radius: 22px;
+  background-color: #e0e5ec;
+  box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.6),
+    inset -9px -9px 16px rgba(255, 255, 255, 0.5);
+  transition: box-shadow 500ms;
+}
+
+.button {
+  width: 25rem;
+  font-weight: bold;
+  padding: 1rem;
+  font-size: 1.1rem;
+  margin: 1rem 0rem;
+  border: none;
+  outline: none;
+  color: #007bff;
+  border-radius: 22px;
+  background-color: #e0e5ec;
+  box-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
+    -9px -9px 16px rgba(255, 255, 255, 0.5);
+  transition: box-shadow 500ms;
+}
+
+.button:hover {
+  box-shadow: inset 9px 9px 16px rgb(163, 177, 198, 0.6),
+    inset -9px -9px 16px rgba(255, 255, 255, 0.5);
+}
+</style>
