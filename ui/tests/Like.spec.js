6b6fbb358a0b704999b31b7107e6b2588074fe92
AbhiRam0099 <abhilashramvaram@gmail.com>
2022-02-25 20:13:53 -0600

    added like button jest unit test

---
+++ ui/tests/Like.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils';
+import LocationCard from './LocationCard';
+
+it('should render the incremented value', () => {
+  const wrapper = mount(LocationCard);
+  wrapper.vm.addOne = jest.fn();
+
+  expect(wrapper.exists()).toBe(true);
+  expect(wrapper.find('button').html()).toBeTruthy();
+  expect(wrapper.vm.addOne).toHaveBeenCalledTimes(1);
+});
+
+
+
