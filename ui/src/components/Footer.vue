81501556aa9dfcc6f49d222508169adf0012439f
NikithaMN-05 <nikithamn30@gmail.com>
2022-03-23 21:41:29 -0500

    added links for swagger, documentation and code coverage

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="container row text-center">
-    <div class="col-sm-3">
+  <div class="container text-center">
+    <div class="row g-0">
+    <div class="col-3">
       <a
         class="link"
         href="https://github.com/pramod096/Featured-Location"
@@ -8,15 +9,7 @@
         >Source</a
       >
     </div>
-    <div class="col-sm-3">
-      <a
-        class="link"
-        href="https://github.com/pramod096/Featured-Location/projects/1"
-        target="_blank"
-        >Board</a
-      >
-    </div>
-    <div class="col-sm-3">
+     <div class="col-3">
       <a
         class="link"
         href="https://github.com/pramod096/Proposal-4B/blob/main/Proposal.md"
@@ -25,13 +18,68 @@
         Proposal</a
       >
     </div>
-    <div class="col-sm-3 link">Group: 4B</div>
+    <div class="col-3">
+      <a
+        class="link"
+        href="https://pramod096.github.io/vuese-featuredlocation-doc/#/"
+        target="_blank"
+        >Documentation</a
+      >
+    </div>
+      <div class="col-3">
+      <a
+        class="link"
+        href="https://github.com/pramod096/Featured-Location/wiki/Group-Organization"
+        target="_blank"
+        >Wiki</a
+      >
+    </div>
+    
+    </div>
+
+    <div class="row g-0">
+    <div class="col-3">
+      <a
+        class="link"
+        href="https://pramod096.github.io/swagger-featuredlocation-doc/"
+        target="_blank"
+        >Swagger</a
+      >
+    </div>
+    <div class="col-3">
+      <a
+        class="link"
+        href="https://www.codacy.com/gh/pramod096/Featured-Location/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=pramod096/Featured-Location&amp;utm_campaign=Badge_Grade"
+        target="_blank"
+        >Codacy</a
+      >
+    </div>
+    <div class="col-3">
+      <a
+        class="link"
+        href="https://snyk.io/test/github/pramod096/Featured-Location"
+        target="_blank"
+      >
+        Snyk</a
+      >
+    </div>
+      <div class="col-3">
+      <a
+        class="link"
+        href="https://pramod096.github.io/jest-testcoverage-featuredlocation/"
+        target="_blank"
+      >
+        Code Coverage</a
+      >
+    </div>
+    </div>
   </div>
+  
 </template>
 
 <style scoped>
-.col-sm-3 {
-  font-size: 0.9rem;
+.col-3 {
+  font-size: 0.65rem;
   font-family: Luminari, fantasy;
   text-shadow: 9px 9px 16px rgb(163, 177, 198, 0.6),
     -9px -9px 16px rgba(255, 255, 255, 0.5);
