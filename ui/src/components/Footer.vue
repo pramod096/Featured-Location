3528d23cc300a8445c39b79213b31f80f8ef7824
NikithaMN-05 <NikithaMN-05@users.noreply.github.com>
2022-03-24 02:41:58 +0000

    Prettified Code!

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -1,80 +1,78 @@
 <template>
   <div class="container text-center">
     <div class="row g-0">
-    <div class="col-3">
-      <a
-        class="link"
-        href="https://github.com/pramod096/Featured-Location"
-        target="_blank"
-        >Source</a
-      >
-    </div>
-     <div class="col-3">
-      <a
-        class="link"
-        href="https://github.com/pramod096/Proposal-4B/blob/main/Proposal.md"
-        target="_blank"
-      >
-        Proposal</a
-      >
-    </div>
-    <div class="col-3">
-      <a
-        class="link"
-        href="https://pramod096.github.io/vuese-featuredlocation-doc/#/"
-        target="_blank"
-        >Documentation</a
-      >
-    </div>
       <div class="col-3">
-      <a
-        class="link"
-        href="https://github.com/pramod096/Featured-Location/wiki/Group-Organization"
-        target="_blank"
-        >Wiki</a
-      >
-    </div>
-    
+        <a
+          class="link"
+          href="https://github.com/pramod096/Featured-Location"
+          target="_blank"
+          >Source</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://github.com/pramod096/Proposal-4B/blob/main/Proposal.md"
+          target="_blank"
+        >
+          Proposal</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://pramod096.github.io/vuese-featuredlocation-doc/#/"
+          target="_blank"
+          >Documentation</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://github.com/pramod096/Featured-Location/wiki/Group-Organization"
+          target="_blank"
+          >Wiki</a
+        >
+      </div>
     </div>
 
     <div class="row g-0">
-    <div class="col-3">
-      <a
-        class="link"
-        href="https://pramod096.github.io/swagger-featuredlocation-doc/"
-        target="_blank"
-        >Swagger</a
-      >
-    </div>
-    <div class="col-3">
-      <a
-        class="link"
-        href="https://www.codacy.com/gh/pramod096/Featured-Location/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=pramod096/Featured-Location&amp;utm_campaign=Badge_Grade"
-        target="_blank"
-        >Codacy</a
-      >
-    </div>
-    <div class="col-3">
-      <a
-        class="link"
-        href="https://snyk.io/test/github/pramod096/Featured-Location"
-        target="_blank"
-      >
-        Snyk</a
-      >
-    </div>
       <div class="col-3">
-      <a
-        class="link"
-        href="https://pramod096.github.io/jest-testcoverage-featuredlocation/"
-        target="_blank"
-      >
-        Code Coverage</a
-      >
-    </div>
+        <a
+          class="link"
+          href="https://pramod096.github.io/swagger-featuredlocation-doc/"
+          target="_blank"
+          >Swagger</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://www.codacy.com/gh/pramod096/Featured-Location/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=pramod096/Featured-Location&amp;utm_campaign=Badge_Grade"
+          target="_blank"
+          >Codacy</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://snyk.io/test/github/pramod096/Featured-Location"
+          target="_blank"
+        >
+          Snyk</a
+        >
+      </div>
+      <div class="col-3">
+        <a
+          class="link"
+          href="https://pramod096.github.io/jest-testcoverage-featuredlocation/"
+          target="_blank"
+        >
+          Code Coverage</a
+        >
+      </div>
     </div>
   </div>
-  
 </template>
 
 <style scoped>
